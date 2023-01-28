48e2cc73067fca8887394820bf6f0ae8a236decf
Andrew Kushnir <akushnir@google.com>
2023-01-28 15:24:40 -0800

    fix: cleanup hydration info from lContainers

--- packages/core/src/render3/instructions/shared.ts
+++ packages/core/src/render3/instructions/shared.ts
@@ -1679,7 +1679,6 @@ function renderComponent(hostLView: LView, componentHostIdx: number) {
   const componentTView = componentView[TVIEW];
   syncViewWithBlueprint(componentTView, componentView);
 
-  // TODO: populate componentView with ngh data from component element.
   const hostRNode = componentView[HOST] as unknown as HTMLElement;
   if (hostRNode !== null) {
     const rawNgh = hostRNode.getAttribute('ngh');
@@ -1688,6 +1687,7 @@ function renderComponent(hostLView: LView, componentHostIdx: number) {
       ngh.firstChild = hostRNode.firstChild as HTMLElement;
       hostRNode.removeAttribute('ngh');
       componentView[HYDRATION_INFO] = ngh;
+      ngDevMode && ngDevMode.hydratedComponents++;
     }
   }
 
