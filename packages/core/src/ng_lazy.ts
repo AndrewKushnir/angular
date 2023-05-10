681914288d871f3717769681c8ca7934051a3ef4
Andrew Kushnir <akushnir@google.com>
2023-05-10 14:27:13 -0700

    WIP: adding loading, placeholder and error templates

--- packages/core/src/ng_lazy.ts
+++ packages/core/src/ng_lazy.ts
@@ -11,8 +11,10 @@
 
 import {inject} from './di/injector_compatibility';
 import {LazyTemplateRef, TemplateRef} from './linker';
+import {createTemplateRef} from './linker/template_ref';
 import {ViewContainerRef} from './linker/view_container_ref';
 import {ɵɵdefineDirective} from './render3';
+import {PARENT, T_HOST} from './render3/interfaces/view';
 
 export class NgLazy {
   static ɵfac = () => new NgLazy();
@@ -20,23 +22,71 @@ export class NgLazy {
     type: NgLazy,
     selectors: [['', 'ngLazy', '']],
     standalone: true,
-    inputs: {loadingTmpl: 'loadingTmpl'},
+    inputs: {
+      loadingTmpl: 'loadingTmpl',
+      placeholderTmpl: 'placeholderTmpl',
+      errorTmpl: 'errorTmpl',
+      when: 'when',
+    },
   });
 
   private vcr = inject(ViewContainerRef);
   private lazyTemplate = inject(LazyTemplateRef);
 
   // @Input() Loading template ref input
-  loadingTmpl?: TemplateRef<unknown>;
+  loadingTmpl?: any;      // TemplateRef<unknown>;
+  placeholderTmpl?: any;  // TemplateRef<unknown>;
+  errorTmpl?: any;        // TemplateRef<unknown>;
+  when: boolean = false;  // when condition
+
+  private previousWhen: boolean|null = null;
 
   constructor() {
     debugger;
     console.log('NgLazy created');
   }
 
-  async ngOnInit(): Promise<void> {
+  // TODO: find a better place for this logic...
+  ngOnChanges() {
     debugger;
-    const templateRef = await this.lazyTemplate.load();
-    this.vcr.createEmbeddedView(templateRef);
+    if (this.previousWhen === null && this.when === false) {
+      // Show placeholder...
+      const placeholderTNode = this.placeholderTmpl[T_HOST];
+      const placeholderLView = this.placeholderTmpl[PARENT];
+      const placeholderTmplRef = createTemplateRef(placeholderTNode, placeholderLView);
+      this.vcr.clear();
+      this.vcr.createEmbeddedView(placeholderTmplRef!);
+
+      this.previousWhen = this.when;
+    } else if (this.previousWhen === false && this.when === true) {
+      // show loading
+      const loadingTNode = this.loadingTmpl[T_HOST];
+      const loadingLView = this.loadingTmpl[PARENT];
+      const loadingTmplRef = createTemplateRef(loadingTNode, loadingLView);
+      this.vcr.clear();
+      this.vcr.createEmbeddedView(loadingTmplRef!);
+      debugger;
+
+      this.lazyTemplate.load()
+          .then(templateRef => {
+            debugger;
+            // Show actual content once everything is loaded...
+            this.vcr.clear();
+            this.vcr.createEmbeddedView(templateRef);
+          })
+          .catch(() => {
+            const errorTNode = this.errorTmpl[T_HOST];
+            const errorLView = this.errorTmpl[PARENT];
+            const errorTmplRef = createTemplateRef(errorTNode, errorLView);
+            this.vcr.clear();
+            this.vcr.createEmbeddedView(errorTmplRef!);
+          });
+      this.previousWhen = this.when;
+    }
+
+    // TODO: consider doing this for some cases in
+    // https://developer.mozilla.org/en-US/docs/Web/API/Window/requestIdleCallback.
+    // const templateRef = await this.lazyTemplate.load();
+    // this.vcr.createEmbeddedView(templateRef);
   }
 }
--- packages/core/test/acceptance/ng_lazy_spec.ts
+++ packages/core/test/acceptance/ng_lazy_spec.ts
@@ -12,35 +12,63 @@ import {TestBed} from '@angular/core/testing';
 import {NgLazy} from '../../src/ng_lazy';
 
 describe('ngLazy directive', () => {
-  fit('should work with basic cases', async () => {
-    @Component({
-      selector: 'my-lazy-cmp',
-      standalone: true,
-      template: 'Hi!',
-    })
-    class MyLazyCmp {
-    }
+  @Component({
+    selector: 'my-lazy-cmp',
+    standalone: true,
+    template: 'Hi!',
+  })
+  class MyLazyCmp {
+  }
 
-    // <my-lazy-cmp />
-    function LazyTemplate(rf: number, ctx: unknown) {
-      if (rf & 1) {
-        element(0, 'my-lazy-cmp');
-      }
+  // <my-lazy-cmp />
+  function LazyTemplate(rf: number, ctx: unknown) {
+    if (rf & 1) {
+      element(0, 'my-lazy-cmp');
     }
+  }
 
-    // Loading...
-    function LoadingTemplate(rf: number, ctx: unknown) {
-      if (rf & 1) {
-        text(0, 'Loading...');
-      }
+  // Loading...
+  function LoadingTemplate(rf: number, ctx: unknown) {
+    if (rf & 1) {
+      text(0, 'Loading...');
     }
+  }
 
-    async function LazyTemplateDeps(): Promise<Array<any>> {
-      return [
-        MyLazyCmp,
-      ];
+  // Placeholder...
+  function PlaceholderTemplate(rf: number, ctx: unknown) {
+    if (rf & 1) {
+      text(0, 'Placeholder!');
     }
+  }
 
+  // Error...
+  function ErrorTemplate(rf: number, ctx: unknown) {
+    if (rf & 1) {
+      text(0, 'Error :(');
+    }
+  }
+
+  async function LazyTemplateDeps(): Promise<Array<any>> {
+    return Promise.allSettled([
+      new Promise((resolve) => {
+        resolve(MyLazyCmp);
+      }),
+    ]);
+  }
+
+  async function LazyErrorTemplateDeps(): Promise<Array<any>> {
+    return Promise.reject(['failed']);
+    // return Promise.all([
+    //   // Promise.reject(MyLazyCmp)
+    //   new Promise((resolve, reject) => {
+    //     reject(MyLazyCmp);
+    //   }),
+    // ]);
+
+    //  Promise.all(new Promise()).then(/* ... */).catch()
+  }
+
+  it('should work with basic cases', async () => {
     /**
      * {#lazy}
      *   <my-lazy-cmp />
@@ -54,30 +82,113 @@ describe('ngLazy directive', () => {
         selectors: [['my-cmp']],
         consts: [[4 as any, 'ngLazy']],
         type: MyCmp,
-        decls: 2,
-        vars: 0,
+        decls: 4,
+        vars: 4,
         standalone: true,
         template:
             function Template(rf: number, ctx: MyCmp) {
               if (rf & 1) {
                 lazy(0, LazyTemplate, LazyTemplateDeps, 1, 0, null, 0);
                 template(1, LoadingTemplate, 1, 0);
+                template(2, PlaceholderTemplate, 1, 0);
+                template(3, ErrorTemplate, 1, 0);
               }
               if (rf & 2) {
                 property('loadingTmpl', ɵɵreference(1));
+                property('placeholderTmpl', ɵɵreference(2));
+                property('errorTmpl', ɵɵreference(3));
+                property('when', ctx.isVisible);
               }
             },
         // TODO: this should be added elsewhere?
         dependencies: [NgLazy],
       });
+
+      isVisible = false;
     }
 
     const fixture = TestBed.createComponent(MyCmp);
     fixture.detectChanges();
 
-    await fixture.whenStable();
-    // flush()?
+    debugger;
 
-    fail(fixture.nativeElement.outerHTML);
+    expect(fixture.nativeElement.outerHTML).toContain('Placeholder');
+
+    fixture.componentInstance.isVisible = true;
+    fixture.detectChanges();
+
+    debugger;
+
+    expect(fixture.nativeElement.outerHTML).toContain('Loading');
+
+    await fixture.whenStable();
+    debugger;
+
+    setTimeout(() => {
+      debugger;
+      expect(fixture.nativeElement.outerHTML).toContain('<my-lazy-cmp>');
+    }, 0);
+  });
+
+  fit('should work with error case', async () => {
+    /**
+     * {#lazy}
+     *   <my-lazy-cmp />
+     * {:loading}
+     *   Loading...
+     * {/#lazy}
+     */
+    class MyCmp {
+      static ɵfac = () => new MyCmp();
+      static ɵcmp = defineComponent({
+        selectors: [['my-cmp']],
+        consts: [[4 as any, 'ngLazy']],
+        type: MyCmp,
+        decls: 4,
+        vars: 4,
+        standalone: true,
+        template:
+            function Template(rf: number, ctx: MyCmp) {
+              if (rf & 1) {
+                lazy(0, LazyTemplate, LazyErrorTemplateDeps, 1, 0, null, 0);
+                template(1, LoadingTemplate, 1, 0);
+                template(2, PlaceholderTemplate, 1, 0);
+                template(3, ErrorTemplate, 1, 0);
+              }
+              if (rf & 2) {
+                property('loadingTmpl', ɵɵreference(1));
+                property('placeholderTmpl', ɵɵreference(2));
+                property('errorTmpl', ɵɵreference(3));
+                property('when', ctx.isVisible);
+              }
+            },
+        // TODO: this should be added elsewhere?
+        dependencies: [NgLazy],
+      });
+
+      isVisible = false;
+    }
+
+    const fixture = TestBed.createComponent(MyCmp);
+    fixture.detectChanges();
+
+    debugger;
+
+    expect(fixture.nativeElement.outerHTML).toContain('Placeholder');
+
+    fixture.componentInstance.isVisible = true;
+    fixture.detectChanges();
+
+    debugger;
+
+    expect(fixture.nativeElement.outerHTML).toContain('Loading');
+
+    await fixture.whenStable();
+    debugger;
+
+    setTimeout(() => {
+      debugger;
+      expect(fixture.nativeElement.outerHTML).toContain('Error');
+    }, 0);
   });
 });
